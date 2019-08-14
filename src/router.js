84e4deb0006ff2756e7d3606d415680d42a1fe07
sarah <sarahkharraz@gmail.com>
2019-08-14 16:49:43 +0100

    Fix basic public routes

--- src/router.js
+++ src/router.js
@@ -1,13 +1,16 @@
 const fs = require("fs");
 const path = require("path");
 const url = require("url");
-const { handleHome, handlePublic } = require("handler.js");
+const { handleHome, handlePublic } = require("./handler.js");
 
 const router = (req, res) => {
   if (req.url === "/") {
     handleHome(req, res);
   }
   if (req.url.startsWith("/public")) {
+  console.log('this is req.url for public', req.url)
     handlePublic(req, res);
   }
 };
+
+module.exports = router;
