1bf6f85f1a2dae7e4234ec664f0ec6e89db56289
gminova <minova1995@gmail.com>
2019-08-15 14:43:43 +0100

    check username not taken function in progress

--- src/handler.js
+++ src/handler.js
@@ -7,6 +7,7 @@ const jwt = require("jsonwebtoken");
 const cookie = require("cookie");
 const bcrypt = require("bcryptjs");
 const querystring = require("querystring");
+const { getData, getUsernames, getHashes } = require("../database/getData");
 
 const handleHome = (req, res) => {
   const filePath = path.join(__dirname, "..", "public/index.html");
@@ -52,8 +53,35 @@ const handleRegister = (req, res) => {
   });
   req.on("end", () => {
     const parsedData = JSON.parse(allData);
-    console.log("JSON DATA: ", JSON.parse(allData));
+    console.log("JSON DATA: ", parsedData);
     console.log("Query string", querystring.parse(allData));
+
+    const username = parsedData.user;
+    const password = parsedData.password;
+
+    //check username is not taken already
+    getUsernames((error, response) => {
+      if(error) {
+        res.writeHead(500, "Content-type: text/html");
+        res.end("<h1>Sorry, unable to get usernames</h1>");
+        console.log(error);
+      } else {
+        //go through array
+        if(username === user_name){
+          //send message Username already taken ERR
+        } else {
+          // encrypt password and resgiter user, send success message
+        let users = JSON.stringify(response);
+        console.log("DB users ", users);
+        res.writeHead(200, {"content-type": "application/json"});
+        res.end(users);
+        }
+      }
+    });
+
+    //return success
+
+    //return message this username is already taken
 
     // Only if registration is successful
 
@@ -63,7 +91,7 @@ const handleRegister = (req, res) => {
 
   // Create JWT
 
-  jwt.sign({ user:  /*USERNAME*/ }, process.env.SECRET);
+  //jwt.sign({ user:  /*USERNAME*/ }, process.env.SECRET);
 
   // encrypt pass
 };
