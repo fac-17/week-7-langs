d15cdad62012bde706fc7e6ba70d5e49fe995166
georgia.shaw <georgia@cranbrookw4.com>
2019-08-15 15:01:44 +0100

    write js to stop user submitting without username or assword

--- public/dom.js
+++ public/dom.js
@@ -13,3 +13,31 @@
 // const registerSubmit = document.querySelector('#register__submit');
 
 // registerSubmit.addEventListener('click', registerUser);
+
+const registerForm = document.querySelector('.register__form__body');
+const usernameInput = document.querySelector('#new__user');
+const passwordInput = document.querySelector('#new__password');
+const retypePasswordInput = document.querySelector('#retype__password');
+let warning = document.createElement('p');
+
+
+registerForm.addEventListener('submit', e => {
+
+if (usernameInput.value === "") {
+  e.preventDefault();
+  warning.textContent = "Please enter a username";
+  registerForm.appendChild(warning);
+}
+
+else if (passwordInput.value === "" || retypePasswordInput.value === "") {
+  e.preventDefault();
+  warning.textContent = "Please enter a password";
+  registerForm.appendChild(warning);
+}
+
+// if (passwordInput.validity.valueMissing || )
+
+
+
+
+});
