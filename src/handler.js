cec92c392681baac07703df6306c2defbd739cf2
Gregor <gregorlthomson@sky.com>
2019-08-15 12:59:40 +0100

    Parse request

--- src/handler.js
+++ src/handler.js
@@ -1,5 +1,12 @@
+/* eslint-disable spaced-comment */
+/* eslint-disable no-unused-vars */
+/* eslint-disable quotes */
 const fs = require("fs");
 const path = require("path");
+const jwt = require("jsonwebtoken");
+const cookie = require("cookie");
+const bcrypt = require("bcryptjs");
+const querystring = require("querystring");
 
 const handleHome = (req, res) => {
   const filePath = path.join(__dirname, "..", "public/index.html");
@@ -37,7 +44,28 @@ const handlePublic = (req, res) => {
 };
 
 const handleRegister = (req, res) => {
+  //Receive info
+  let allData = "";
 
-}
+  req.on("data", chunk => {
+    allData += chunk;
+  });
+  req.on("end", () => {
+    const parsedData = JSON.parse(allData);
+    console.log("JSON DATA: ", JSON.parse(allData));
+    console.log("Query string", querystring.parse(allData));
+
+    // Only if registration is successful
+
+    res.writeHead(302, { Location: "/about" });
+    res.end();
+  });
+
+  // Create JWT
+
+  jwt.sign({ user:  /*USERNAME*/ }, process.env.SECRET);
+
+  // encrypt pass
+};
 
 module.exports = { handleHome, handlePublic, handleRegister };
--- src/router.js
+++ src/router.js
@@ -1,21 +1,22 @@
-const fs = require('fs');
-const path = require('path');
-const url = require('url');
-const { handleHome, handlePublic, handleRegister } = require('./handler.js');
+const fs = require("fs");
+const path = require("path");
+const url = require("url");
+const { handleHome, handlePublic, handleRegister } = require("./handler.js");
 
 const router = (req, res) => {
-  if (req.url === '/') {
+  if (req.url === "/") {
     handleHome(req, res);
-  } else if (req.url.startsWith('/public')) {
+  } else if (req.url.startsWith("/public")) {
     handlePublic(req, res);
-  } else if(req.url.startsWith("/register")) {
+  } else if (req.url.startsWith("/register") && req.method === "POST") {
     handleRegister(req, res);
-  } else {      
-    const notFound = '<p style="font-size: 10vh; text-align: center;">404, Sorry, this page doesn\'t exist!</p>';
+  } else {
+    const notFound =
+      '<p style="font-size: 10vh; text-align: center;">404, Sorry, this page doesn\'t exist!</p>';
     res.writeHead(404, {
-     "Content-type": "text/html",
-     "Content-length": notFound.length
-    })
+      "Content-type": "text/html",
+      "Content-length": notFound.length
+    });
     res.end(notFound);
   }
 };
