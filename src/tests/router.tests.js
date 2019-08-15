4b0973bd5435b12ab5054a63f8d058e0d160d386
Sam Harris <sam@jam-bc.com>
2019-08-15 11:09:42 +0100

    demo tests are running

--- src/tests/router.tests.js
+++ src/tests/router.tests.js
@@ -1,6 +1,10 @@
-import tape from 'tape';
-import supertest from 'supertest';
-import router from '../router';
+const tape = require('tape');
+
+const supertest = require('supertest');
+const router = require('../router');
 
 
-tape('Router tests');
+tape('running tape', (t) => {
+  t.equal(1, 1, 'tests are running. wahoo');
+  t.end();
+});
