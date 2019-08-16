9f28353b937842de208533bd59f10e5b99791cf4
gminova <minova1995@gmail.com>
2019-08-16 12:17:35 +0100

    reverted router, tests passing

--- src/router.js
+++ src/router.js
@@ -1,30 +1,33 @@
 const fs = require("fs");
 const path = require("path");
 const url = require("url");
-const { handleHome, handlePublic, handleRegister } = require("./handler.js");
-const log = require('./loggedIn')
+const {
+  handleHome,
+  handlePublic,
+  handleRegister,
+  handleLogin,
+  handleAbout
+} = require("./handler.js");
 
 const router = (req, res) => {
-  const logged_in = log(req, res)
-  if(logged_in instanceof Error) return;
-  if(logged_in){
-
-    if (req.url === "/") {
-      handleHome(req, res);
-    } else if (req.url.startsWith("/public")) {
-      handlePublic(req, res);
-    } else if (req.url.startsWith("/register")) {
-      console.log("i'm in router register");
-      handleRegister(req, res);
-    } else {
-      const notFound =
-        '<p style="font-size: 10vh; text-align: center;">404, Sorry, this page doesn\'t exist!</p>';
-      res.writeHead(404, {
-        "Content-type": "text/html",
-        "Content-length": notFound.length
-      });
-      res.end(notFound);
-    }  
+  if (req.url === "/") {
+    handleHome(req, res);
+  } else if (req.url.startsWith("/public")) {
+    handlePublic(req, res);
+  } else if (req.url.startsWith("/register")) {
+    handleRegister(req, res);
+  } else if (req.url.startsWith("/login")) {
+    handleLogin(req, res);
+  } else if (req.url.startsWith("/about")) {
+    handleAbout(req, res);
+  } else {
+    const notFound =
+      '<p style="font-size: 10vh; text-align: center;">404, Sorry, this page doesn\'t exist!</p>';
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "Content-length": notFound.length
+    });
+    res.end(notFound);
   }
 };
 
